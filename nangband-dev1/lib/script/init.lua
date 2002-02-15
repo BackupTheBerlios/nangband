c7cd85753fc299b30b0a058ce6a7163273ba425c
takkaria <takkaria>
2002-02-15 09:39:37 +0000

    Added a note about the borg scripting patch.

--- nangband-dev1/lib/script/init.lua
+++ nangband-dev1/lib/script/init.lua
@@ -5,4 +5,8 @@ end
 
 dofile(build_script_path("object.lua"))
 dofile(build_script_path("spell.lua"))
-defile(build_script_patch("borg.lua"))
+
+-- Note to porters: only uncomment this line if you have defined
+-- ALLOW_BORG *and* BORG_SCRIPTING.
+
+-- dofile(build_script_patch("borg.lua"))

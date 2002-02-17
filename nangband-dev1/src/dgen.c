9430dcec8e9db87b6c99e6bddea6cdd1648f4b6c
takkaria <takkaria>
2002-02-17 20:30:48 +0000

    Commited a non-working dgen.c.

--- nangband-dev1/src/dgen.c
+++ nangband-dev1/src/dgen.c
@@ -8,9 +8,29 @@
  */
 
 #include "angband.h"
+#include "dtype.h"
 
-static void generate_dungeon(map_grid_t *dungeon, int width, int height, int level, int exeption)
+static void generate_dungeon(map_grid_t **dungeon, int width, int height, int level, int exeption)
 {
+	map_grid_t *this_grid;
+	int x, y;
+	
+	/* Debugging info */
+	printf("args are: width = %i, height = %i, level = %i, exeption = %i\n", width, height, level, exeption);
+
+	/* Init the dungeon to empty spaces */
+	for (x = 0; x < width; x++)
+	{
+		for (y = 0; y < height; y++)
+		{
+			printf("x = %i, y = %i", x, y);
+			*dungeon->feature = TERRAIN_TYPE_WALL;
+		}
+	}
+	
+	/* Pick a random point in the dungeon */
+	this_grid = dungeon[rand_int(width)][rand_int(height)];
+	
 	/* Return control */
 	return;
 }

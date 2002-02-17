0b65f5c4df3333f9483671fe7cc6f9e2af7f0ca0
takkaria <takkaria>
2002-02-17 20:05:46 +0000

    Started work on the dungeon generator.

--- nangband-dev1/src/dgen.c
+++ nangband-dev1/src/dgen.c
@@ -6,5 +6,24 @@
  * This file is dual-licenced under both the Angband and GPL licences.
  * It is currently distribued under the Angband licence.
  */
- 
- #include "angband.h"
+
+#include "angband.h"
+
+static void generate_dungeon(map_grid_t *dungeon, int width, int height, int level, int exeption)
+{
+	/* Return control */
+	return;
+}
+
+#ifdef DEBUG
+
+int main(int argc, char **argv)
+{
+	map_grid_t dungeon[80][80];
+	
+	generate_dungeon(&dungeon, 80, 80, 2, 0);
+	
+	return(0);
+}
+
+#endif /* DEBUG */

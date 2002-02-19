b4a1488aba1824cbff98f06c329b2b9745e4f885
takkaria <takkaria>
2002-02-19 17:20:58 +0000

    Removing the file.

--- nangband-dev1/src/l-ui.c
+++ nangband-dev1/src/l-ui.c
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: ui
-** Generated automatically by tolua 4.0a - angband on Mon Feb 18 21:28:37 2002.
+** Generated automatically by tolua 4.0a - angband on Tue Feb 19 17:18:17 2002.
 */
 
 #include "lua/tolua.h"

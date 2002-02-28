e49e7ea97097e4e627d711ddaadd10a457660b6e
takkaria <takkaria>
2002-02-28 21:04:52 +0000

    Altered various makefiles to compile in the new resist.c file.

--- nangband-dev1/src/defines.h
+++ nangband-dev1/src/defines.h
@@ -1,12 +1,9 @@
 /*
  * File: defines.h
  * Purpose: Various constants and macros for Angband
- *
  * Copyright (c) 1997 Ben Harrison, James E. Wilson, Robert A. Koeneke
  *
- * This software may be copied and distributed for educational, research,
- * and not for profit purposes provided that this copyright and statement
- * are included in all such copies.  Other copyrights may also apply.
+ * This file is distributed under the Angband licence.
  */
 
 

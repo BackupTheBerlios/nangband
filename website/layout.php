e28e0312c781fbe67afacb6b8b092de74a63c9cc
takkaria <takkaria>
2002-03-07 21:00:38 +0000

    Added the basic files.

---
+++ website/layout.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * layout.php - page layout functions
+ * ----------------------------------
+ *
+ * started on:		07/03/02
+ * by:				takkaria
+ */
+
+function page_header($title, $type)
+{
+	echo '<html>';
+	echo '<head><title>Nangband - ' . $title . '</title></head>';
+	echo '<body bgcolor="#000000" text="#ffffff">';
+	echo '<h1>' . $title . '</h1>';
+	echo '<hr width="50%" align="center">'
+}
+
+function page_footer()
+{
+	echo '<hr width="50%" align="center">'
+	echo '<div align="center">Nangband website. Contact ';
+	echo '<a href="mailto:nevermiah@hotmail.com">nevermiah@hotmail.com</a>';
+	echo '</div>';
+	echo '</body></html>';
+}

59ce1a613c7a3745cf77be8a451d9700222d5d25
takkaria <takkaria>
2002-05-02 17:53:27 +0000

    Fixed typos.

--- website/layout.php
+++ website/layout.php
@@ -24,25 +24,25 @@ function do_links()
 
 function page_header($title, $type)
 {
-	echo '<html>\n';
-	echo '<head>\n';
-	echo '<title>Nangband - ' . $title . '</title>\n';
+	echo '<html>';
+	echo '<head>';
+	echo '<title>Nangband - ' . $title . '</title>';
 /*	echo '<link rel="stylesheet" rev="content" type="text/css" href="style.css">'; */
-	echo '<meta name="description" content="A site about Nangband, an Angband variant.">\n';
-	echo '</head>\n';
-	echo "<body bgcolor="#ffffee">\n";
-	echo '<table width="95%" align="center" callspacing="1" cellpadding="0" border="0">\n';
-	echo '<tr><td>\n';
-	echo '<table width="100%" align="center" callspacing="0" cellpadding="4" border="0">\n';
+	echo '<meta name="description" content="A site about Nangband, an Angband variant.">';
+	echo '</head>';
+	echo '<body bgcolor="#ffffee">';
+	echo '<table width="95%" align="center" callspacing="1" cellpadding="0" border="0">';
+	echo '<tr><td>';
+	echo '<table width="100%" align="center" callspacing="0" cellpadding="4" border="0">';
 	echo '<tr><td bgcolor="#eeeeee" align="center"><font face="Veranda" color="#555555" size="+2">';
 
-	echo '<b>Nangband - '.$title.'</b></font>\n';
+	echo '<b>Nangband - '.$title.'</b></font>';
 
-	echo '<br>\n';
+	echo '<br>';
 	echo '<font face="Veranda" color="#555555">';
 	do_links();
-	echo '</font>\n';
-	echo '</td></tr>\n';
+	echo '</font>';
+	echo '</td></tr>';
 
 	echo '<tr><td bgcolor="#dddddd"><font face="Veranda" color="#333333">';
 
@@ -60,8 +60,8 @@ function page_footer($creators)
 	echo '<font size="-1"><div align="center">All material is &copy; ' . $creators . '. Contact ';
 	echo '<a href="mailto:nevermiah@hotmail.com">nevermiah@hotmail.com</a>';
 
-	echo '</font></td></tr>\n';
-	echo '</table></td></tr></table>\n';
+	echo '</font></td></tr>';
+	echo '</table></td></tr></table>';
 
 	echo '</body></html>';
 

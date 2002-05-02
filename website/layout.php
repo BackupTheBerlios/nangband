9c9e9b0d7b87828b1465006a3175b5304bbb7411
takkaria <takkaria>
2002-05-02 17:50:30 +0000

    changed the layout completely.

--- website/layout.php
+++ website/layout.php
@@ -7,38 +7,62 @@
  * by:				takkaria
  */
 
-echo '<html><head><title>Access Denied.<title></head><body>Access Denied.</body></html>';
-
-function page_header($title, $type)
+/*if ($access != 'eric')
 {
-	echo '<html>';
-	echo '<head>';
-	echo '<title>Nangband - ' . $title . '</title>';
-	echo '<link rel="stylesheet" rev="content" type="text/css" href="style.css">';
-	echo '<meta name="description" content="A site about Nangband, an Angband variant.">';
-	echo '</head>';
-	echo '<body bgcolor="#000000" text="#ffffff" link="#d090ff" alink="#00ffff" vlink="#77ff44">';
-	echo '<h1 align="center"><img src="images/nangband.gif" align="center"><br>'.$title.'</h1>';
+echo '<html><head><title>Access Denied.</title></head><body>Access Denied.</body></html>';
+}*/
 
-	echo '<div align="center">';
+function do_links()
+{
 	echo '[ <a href="index.php">Main Page</a> | ';
 	echo '<a href="changes.php">Changes</a> | ';
 	echo '<a href="download.php">Download</a> | ';
-	echo '<a href="http://developer.berlios.de/projects/nangband">Project Page</a> ] ';
-	echo '</div>';
+	echo '<a href="http://developer.berlios.de/projects/nangband">Project Page</a> ]';
 
-	echo '<hr width="450" size="1" align="center">';
-	echo '<br>';
+	return;
+}
+
+function page_header($title, $type)
+{
+	echo '<html>\n';
+	echo '<head>\n';
+	echo '<title>Nangband - ' . $title . '</title>\n';
+/*	echo '<link rel="stylesheet" rev="content" type="text/css" href="style.css">'; */
+	echo '<meta name="description" content="A site about Nangband, an Angband variant.">\n';
+	echo '</head>\n';
+	echo '<body bgcolor="#ffffee">\n';
+	echo '<table width="95%" align="center" callspacing="1" cellpadding="0" border="0">\n';
+	echo '<tr><td>\n';
+	echo '<table width="100%" align="center" callspacing="0" cellpadding="4" border="0">\n';
+	echo '<tr><td bgcolor="#eeeeee" align="center"><font face="Veranda" color="#555555" size="+2">';
+
+	echo '<b>Nangband - '.$title.'</b></font>\n';
+
+	echo '<br>\n';
+	echo '<font face="Veranda" color="#555555">';
+	do_links();
+	echo '</font>\n';
+	echo '</td></tr>\n';
+
+	echo '<tr><td bgcolor="#dddddd"><font face="Veranda" color="#333333">';
 
 	return;
 }
 
 function page_footer($creators)
 {
-	echo '<hr width="50%" size="1" align="center">';
+	echo '</font></td></tr>\n';
+ 
+	echo '<tr><td bgcolor="#eeeeee" align="center">';
+	echo '<font face="Veranda" color="#555555">';
+	do_links();
+	echo '<br>';
 	echo '<font size="-1"><div align="center">All material is &copy; ' . $creators . '. Contact ';
 	echo '<a href="mailto:nevermiah@hotmail.com">nevermiah@hotmail.com</a>';
-	echo '</div></font>';
+
+	echo '</font></td></tr>\n';
+	echo '</table></td></tr></table>\n';
+
 	echo '</body></html>';
 
 	return;

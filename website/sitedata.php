530fe55d8d682ee7cea7534fda5017619918961e
takkaria <takkaria>
2002-07-07 09:21:33 +0000

    Added files.php - it's vital to the site now.

---
+++ website/sitedata.php
@@ -0,0 +1,17 @@
+<?php
+
+$titles['main'] = 'main page';
+$titles['changes'] = 'changes';
+$titles['devinfo'] = 'development information';
+$titles['download'] = 'downloads';
+$titles['links'] = 'links';
+
+$pages[0] = 'main';
+$pages[1] = 'changes';
+$pages[2] = 'devinfo';
+$pages[3] = 'download';
+$pages[4] = 'links';
+
+$no_pages = 5;
+
+?>
